499172da4cec73aae3ee49b69d2b4171d69bf1cd
Brian Mock <brian@mockbrian.com>
2020-03-08 12:45:30 -0700

    website now :)

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 pixelated replacement folder icons for Windows and Mac
 
-1. [Download the zip](./dist/pixel-folders.zip)
-2. Open the zip
-3. Follow the instructions in `README.txt`
+Go to <https://wavebeem-pixel-folders.netlify.com>.
---
+++ components/icon-preview.js
@@ -0,0 +1,17 @@
+class IconPreview extends HTMLElement {
+  connectedCallback() {
+    const content = document
+      .getElementById("template-icon-preview")
+      .content.cloneNode(true);
+    const size = Number(this.getAttribute("size") || 16);
+    console.log("Whatup", size);
+    this.innerHTML = "";
+    for (const img of content.querySelectorAll("img")) {
+      img.width = size;
+      img.height = size;
+    }
+    this.appendChild(content);
+  }
+}
+
+customElements.define("icon-preview", IconPreview);
