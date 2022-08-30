aaa341bf6c1b94a331fd5705475f3af61eeadf9e
Brian Mock <brian@wavebeem.com>
2022-08-29 18:50:52 -0700

    AAP-64 colors

--- bin/build.sh
+++ bin/build.sh
@@ -6,34 +6,36 @@ Main() {
   mkdir -p build/{mac,windows} dist
   cd build
   for file in ../src/*.png; do
-    local name="$(basename "${file%%.png}")"
+    local name date year
+    name="$(basename "${file%%.png}")"
     printf "  %-30s [" "$name"
     for size in 16 32 48 64 128 256; do
       echo -n "."
-      convert "$file" -scale "$size" "icon_${size}x${size}.png"
+      magick "$file" -scale "$size" -strip "icon_${size}x${size}.png"
       mkdir -p "png/$size"
       cp "icon_${size}x${size}.png" "png/${size}/${name}.png"
     done
     echo -n "."
-    convert *.png icon.ico
+    magick ./*.png -strip icon.ico
     mv icon.ico "windows/$name.ico"
     echo -n "."
     mkdir icon.iconset
-    mv *.png icon.iconset
+    mv ./*.png icon.iconset
     iconutil --convert icns -o "mac/$name.icns" icon.iconset
-    rm -rf icon.iconset *.png
+    rm -rf icon.iconset ./*.png
     echo "]"
   done
-  local date="$(date -u "+%Y-%m-%d %H:%M:%S UTC")"
-  local year="$(date "+%Y")"
+  date="$(date -u "+%Y-%m-%d %H:%M:%S UTC")"
+  year="$(date "+%Y")"
   cp ../LICENSE.txt .
-  cat ../src/README-template.txt \
-    | sed "s/{year}/$year/g" \
-    | sed "s/{date}/$date/g" \
+  sed \
+    -e "s/{year}/$year/g" \
+    -e "s/{date}/$date/g" \
+    ../src/README-template.txt \
     > README.txt
   echo
-  zip -r pixel-folders.zip *
-  mv * ../dist
+  zip -r pixel-folders.zip ./*
+  mv ./* ../dist
   echo
   cd ..
   rm -rf build
